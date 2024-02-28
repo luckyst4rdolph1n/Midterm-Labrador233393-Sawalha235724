a2f37b5a1069dee4edcdb2d242c7a5172ed72daa
zhcocozhan <161426992+zhcocozhan@users.noreply.github.com>
2024-02-28 14:07:49 +0800

    Update Pentagon.java

--- Pentagon.java
+++ Pentagon.java
@@ -18,15 +18,11 @@ public class Pentagon implements DrawingObject{
     public void draw(Graphics2D g2d){
         Path2D.Double p = new Path2D.Double();
         p.moveTo(x, y);
-        p.lineTo(x-10, y);
-        p.lineTo((x-10)+(10*0.31),y+(0.95*(10)));
-        p.lineTo((2*x-10)/2,y+(1.54*10));
-        p.lineTo(x+3.1,y+(0.95*(10)));
+        p.lineTo(x+10, y);
+        p.lineTo(x+13.1 ,y+9.5);
+        p.lineTo((2*x+10)/2,y+15.4);
+        p.lineTo(x-3.1,y+9.5);
         p.closePath();
-        //Line2D.Double l1 = new Line2D.Double(x,y,x-3,y);
-        //Line2D.Double l2 = new Line2D.Double(x-3,y,(x-3)*1.31,(y-0.95)*(x-3));
-        //Line2D.Double l3 = new Line2D.Double((x-3)*1.31,(y-0.95)*(x-3),(2*x-3)/2,(y-1.54)*(x-3));
-        //Line2D.Double l4 = new Line2D.Double((2*x-3)/2,(y-1.54)*(x-3),);
         g2d.setColor(color);
         g2d.fill(p);
     }
