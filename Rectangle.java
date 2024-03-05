2dca665b4f9713b03ff05667f6e459a7dde45d8f
zhcocozhan <161426992+zhcocozhan@users.noreply.github.com>
2024-03-06 05:17:08 +0800

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,3 +1,23 @@
+/**
+	This Rectangle class is for drawing  a rectangle shape.
+    It is a convenient way to create rectangle and minimize code lines.
+	
+	@author Zandalee Beck Q. Labrador (233393); Shamika Anne E. Sawalha (235724) 
+	@version 26 February 2024
+	
+	I have not discussed the Java language code in my program 
+	with anyone other than my instructor or the teaching assistants 
+	assigned to this course.
+
+	I have not used Java language code obtained from another student, 
+	or any other unauthorized source, either modified or unmodified.
+
+	If any Java language code or documentation used in my program 
+	was obtained from another source, such as a textbook or website, 
+	that has been clearly noted with a proper citation in the comments 
+	of my program.
+**/
+
 import java.awt.*;
 import java.awt.geom.*;
 
@@ -9,6 +29,7 @@ public class Rectangle implements DrawingObject{
     double height;
     Color color;
 
+    //sets the rectangle's position, size, and color
     public Rectangle(double x, double y, double width, double height, Color color){
         this.x = x;
         this.y = y;
@@ -18,6 +39,7 @@ public class Rectangle implements DrawingObject{
         
     }
 
+    //overrides the draw method
     @Override
     public void draw(Graphics2D g2d){
         Rectangle2D.Double rectangle = new Rectangle2D.Double(x, y, width, height);
