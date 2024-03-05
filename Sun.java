f9ee5b2e2428276a455a47997ead694d2d996ffc
zhcocozhan <161426992+zhcocozhan@users.noreply.github.com>
2024-03-06 04:53:00 +0800

    Update Sun.java

--- Sun.java
+++ Sun.java
@@ -1,3 +1,23 @@
+/**
+	This Sun class is for drawing the dog.
+    It draws the dog in its default state and in its barking state.
+	
+	@author Zandalee Beck Q. Labrador (233393); Shamika Anne E. Sawalha (235724) 
+	@version 2 March 2024
+	
+	I have not discussed the Java language code in my program 
+	with anyone other than my instructor or the teaching assistants 
+	assigned to this course.
+
+	I have not used Java language code obtained from another student, 
+	or any other unauthorized source, either modified or unmodified.
+
+	If any Java language code or documentation used in my program 
+	was obtained from another source, such as a textbook or website, 
+	that has been clearly noted with a proper citation in the comments 
+	of my program.
+**/
+
 import java.awt.*;
 import java.awt.geom.*;
 
@@ -21,16 +41,18 @@ public class Sun implements DrawingObject{
         this.sunColor = sunColor;
     }
 
-    public void generateSun(){
+    public void generateSun(){ //dtaws the body of the sun and a ray
         sunBody = new Circle(x, y, size, sunColor, new Color(0, 0, 0 ,0));
         sunLine = new Line(((x+size)+x)/2, y-5,((x+size)+x)/2, y-40, 1, sunColor);
     }
 
+    //overrides the draw method
     @Override
     public void draw(Graphics2D g2d){
         generateSun();
         AffineTransform reset = g2d.getTransform();
 
+        //the movement of the sun's glow as it is animated
         if(state){
             sunGlow = new Circle(x-15, y-15, size+30, new Color(255, 233, 95, 100), new Color(0, 0, 0 ,0));
             sunGlow.draw(g2d);
@@ -42,6 +64,7 @@ public class Sun implements DrawingObject{
         sunBody.draw(g2d);
         sunLine.draw(g2d);
 
+        //the following loop is for creating multiple rays around the sun
         int degs = 30;
         for(int i=0; i<11; i++){
             g2d.setTransform(reset);
@@ -53,6 +76,7 @@ public class Sun implements DrawingObject{
         g2d.setTransform(reset);
     }
 
+    //animating the glow by alternating the states
     public void glow(){
         if (state){
             state = false;
