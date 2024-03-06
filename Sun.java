0e500a177d895e2647efd6ac28f0fb5c3b407d5e
shamika <shamanne.sawalha@gmail.com>
2024-03-06 09:17:30 +0800

    modified javadoc

--- Sun.java
+++ Sun.java
@@ -1,10 +1,10 @@
 /**
 	This Sun class is for drawing the sun.
-    	The sun only appears in day moded.
-     	This class also contains the foundation of animating the sun's glow.
+    It can only be seen during the day mode.
+    This class also contains the method for animating the Sun object's glow.
 	
 	@author Zandalee Beck Q. Labrador (233393); Shamika Anne E. Sawalha (235724) 
-	@version 2 March 2024
+	@version 27 February 2024
 	
 	I have not discussed the Java language code in my program 
 	with anyone other than my instructor or the teaching assistants 
@@ -35,6 +35,13 @@ public class Sun implements DrawingObject{
 
     private Boolean state = true;
 
+    /**
+     * Constructor of the Sun class.
+     * @param x
+     * @param y
+     * @param size
+     * @param sunColor
+     */
     public Sun(double x, double y, double size, Color sunColor){
         this.x = x;
         this.y = y;
@@ -42,18 +49,24 @@ public class Sun implements DrawingObject{
         this.sunColor = sunColor;
     }
 
-    public void generateSun(){ //dtaws the body of the sun and a ray
+    /**
+     * Instantiates the parts of a Sun object.
+     */
+    public void generateSun(){
         sunBody = new Circle(x, y, size, sunColor, new Color(0, 0, 0 ,0));
         sunLine = new Line(((x+size)+x)/2, y-5,((x+size)+x)/2, y-40, 1, sunColor);
     }
 
-    //overrides the draw method
+    /*
+     * Overridden method of the DrawingObject interface.
+     * Renders the Sun object.
+     */
     @Override
     public void draw(Graphics2D g2d){
         generateSun();
         AffineTransform reset = g2d.getTransform();
 
-        //the movement of the sun's glow as it is animated
+        //resizes the glow component of the Sun object for animation
         if(state){
             sunGlow = new Circle(x-15, y-15, size+30, new Color(255, 233, 95, 100), new Color(0, 0, 0 ,0));
             sunGlow.draw(g2d);
@@ -65,7 +78,7 @@ public class Sun implements DrawingObject{
         sunBody.draw(g2d);
         sunLine.draw(g2d);
 
-        //the following loop is for creating multiple rays around the sun
+        // creates the lines around the sun by 30deg increments
         int degs = 30;
         for(int i=0; i<11; i++){
             g2d.setTransform(reset);
@@ -77,7 +90,9 @@ public class Sun implements DrawingObject{
         g2d.setTransform(reset);
     }
 
-    //animating the glow by alternating the states
+    /**
+     * Switches the state of the Sun object.
+     */
     public void glow(){
         if (state){
             state = false;
