df60d056ce8b73d4bde3b9ab0c56e65920f32453
shamika <shamanne.sawalha@gmail.com>
2024-03-06 09:29:40 +0800

    modified, added javadoc

--- SceneFrame.java
+++ SceneFrame.java
@@ -1,3 +1,25 @@
+/**
+	This class contains the components of the JFrame.
+    It creates an instance of the SceneCanvas class.
+    Also contains and implements the ActionListeners 
+    for the Cloud, Sun, Star, and Moon objects of the program.
+	
+	@author Zandalee Beck Q. Labrador (233393); Shamika Anne E. Sawalha (235724) 
+	@version 5 March 2024
+	
+	I have not discussed the Java language code in my program 
+	with anyone other than my instructor or the teaching assistants 
+	assigned to this course.
+
+	I have not used Java language code obtained from another student, 
+	or any other unauthorized source, either modified or unmodified.
+
+	If any Java language code or documentation used in my program 
+	was obtained from another source, such as a textbook or website, 
+	that has been clearly noted with a proper citation in the comments 
+	of my program.
+**/
+
 import java.awt.event.*;
 import javax.swing.*;
 
@@ -10,6 +32,11 @@ public class SceneFrame{
     private SceneCanvas sc;
     public boolean barked, day, night;
 
+    /**
+     * Constructor for SceneFrame class.
+     * @param w
+     * @param h
+     */
     public SceneFrame(int w, int h){
         frame = new JFrame();
         w = width;
@@ -17,6 +44,9 @@ public class SceneFrame{
         sc = new SceneCanvas(800, 600);
     }
 
+    /**
+     * Configures the JFrame and adds the JComponents to display them.
+     */
     public void setUpGUI(){
         frame.setTitle("Midterm Project - Labrador - Sawalha");
         frame.add(sc);
@@ -24,16 +54,20 @@ public class SceneFrame{
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
         cloudTimer.start();
-        sunGlowTimer.start();
+        GlowTimer.start();
         starTwinkleTimer.start();
 
         frame.addKeyListener(sc.dogBase.forBark);
         frame.addKeyListener(sc.daybg.forDay);
         frame.addKeyListener(sc.nightbg.forNight);
-        frame.addKeyListener(sc.wm.disappear);
 
     }
 
+    /**
+     * Anonymous class that creates the ActionListener for
+     * the movement of Cloud Objects.
+     * Repaints the canvas.
+     */
     ActionListener cloudActionListener = new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent ae){
@@ -45,14 +79,25 @@ public class SceneFrame{
         }
     };
 
-    ActionListener sunGlowActionListener = new ActionListener() {
+    /**
+     * Anonymous class that creates the ActionListener for
+     * the changing states of the Sun and Moon objects.
+     * Repaints the canvas.
+     */
+    ActionListener GlowActionListener = new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent ae){
             sc.getSun().glow();
+            sc.getMoon().glow();
             sc.repaint();
         }
     };
 
+    /**
+     * Anonymous class that creates the ActionListener for
+     * the changing states of the Star objects.
+     * Repaints the canvas.
+     */
     ActionListener starTwinkleActionListener = new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent ae){
@@ -69,8 +114,12 @@ public class SceneFrame{
         }
     };
 
+    /**
+     * Timers for the speed of movement and state switching.
+     * Each assigned to their own ActionListener.
+     */
     Timer cloudTimer = new Timer(0, cloudActionListener);
-    Timer sunGlowTimer = new Timer(500, sunGlowActionListener);
+    Timer GlowTimer = new Timer(500, GlowActionListener);
     Timer starTwinkleTimer = new Timer(600, starTwinkleActionListener);
     
     }
