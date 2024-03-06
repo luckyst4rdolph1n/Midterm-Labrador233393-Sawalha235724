ba1d96384da5cd8b169a5ae9fda0a1183575e6b3
shamika <shamanne.sawalha@gmail.com>
2024-03-06 08:27:21 +0800

    modified, added javadoc

--- Trees.java
+++ Trees.java
@@ -1,3 +1,23 @@
+/**
+	This class creates a Tree object.
+    It is responsible for the position and colors of a Tree object.
+	
+	@author Zandalee Beck Q. Labrador (233393); Shamika Anne E. Sawalha (235724) 
+	@version 29 February 2024
+	
+	I have not discussed the Java language code in my program 
+	with anyone other than my instructor or the teaching assistants 
+	assigned to this course.
+
+	I have not used Java language code obtained from another student, 
+	or any other unauthorized source, either modified or unmodified.
+
+	If any Java language code or documentation used in my program 
+	was obtained from another source, such as a textbook or website, 
+	that has been clearly noted with a proper citation in the comments 
+	of my program.
+**/
+
 import java.awt.*;
 import java.awt.geom.*;
 
@@ -5,22 +25,38 @@ public class Trees implements DrawingObject{
     
     private double x;
     private double y;
+    private Color trunkColor, crownColor1, crownColor2, crownColor3, shadowColor1, shadowColor2;
 
     private Triangle trunk;
-    private Path2D.Double topCrown;
-    private Path2D.Double sideCrown;
+    private Path2D.Double topCrown, sideCrown, branches, shadow;
 
-    private Path2D.Double branches;
-    private Path2D.Double shadow;
-
-
-    public Trees(double x, double y){
+    /**
+     * Constructor for the Tree class.
+     * @param x
+     * @param y
+     * @param trunkColor
+     * @param crownColor1
+     * @param crownColor2
+     * @param crownColor3
+     * @param shadowColor1
+     * @param shadowColor2
+     */
+    public Trees(double x, double y, Color trunkColor, Color crownColor1, Color crownColor2, Color crownColor3, Color shadowColor1, Color shadowColor2 ){
         this.x = x;
         this.y = y;
+        this.trunkColor = trunkColor;
+        this.crownColor1 = crownColor1;
+        this.crownColor2 = crownColor2;
+        this.crownColor3 = crownColor3;
+        this.shadowColor1 = shadowColor1;
+        this.shadowColor2 = shadowColor2;
     }
 
+    /**
+     * Instantiates the parts of a Tree object.
+     */
     public void generateTreeParts(){
-        trunk = new Triangle(x, y, 200, 50,  new Color(108, 60, 31));
+        trunk = new Triangle(x, y, 200, 50, trunkColor);//new Color(108, 60, 31)
         
         topCrown = new Path2D.Double();
         topCrown.moveTo(x-50, y-150);
@@ -51,41 +87,44 @@ public class Trees implements DrawingObject{
 
     }
 
+    /**
+     * Overridden method of the DrawingObject interface.
+     * Renders a Tree object.
+     */
     @Override
     public void draw(Graphics2D g2d){
         generateTreeParts();
         AffineTransform reset = g2d.getTransform();
         trunk.draw(g2d);
-        g2d.setColor(new Color(108, 60, 31));
+        g2d.setColor(trunkColor);//Color(108, 60, 31)
         g2d.setStroke(new BasicStroke(10));
         g2d.draw(branches);
         
-        g2d.setColor(new Color(0, 139, 50));
+        g2d.setColor(crownColor1);//new Color(0, 139, 50)
         g2d.fill(topCrown);
 
-        g2d.setColor(new Color(0, 162, 50));
+        g2d.setColor(shadowColor1); //new Color(0, 162, 50)
         g2d.fill(shadow);
 
         g2d.translate(45, 0);
-        g2d.setColor(new Color(1, 104, 32));
+        g2d.setColor(crownColor2);//new Color(1, 104, 32)
         g2d.fill(sideCrown);
 
         g2d.scale(0.8, 0.8);
         g2d.translate(30, 97);
-        g2d.setColor(new Color(0, 124, 36));
+        g2d.setColor(shadowColor2);//new Color(0, 124, 36)
         g2d.fill(shadow);
         g2d.setTransform(reset);
-        
 
         g2d.scale(0.8, 0.8);
         g2d.translate(-60, 90);
-        g2d.setColor(new Color(0, 105, 37));
+        g2d.setColor(crownColor3);//new Color(0, 105, 37)
         g2d.fill(sideCrown);
         g2d.setTransform(reset);
 
         g2d.scale(0.6, 0.6);
         g2d.translate(-40, 239);
-        g2d.setColor(new Color(0, 124, 36));
+        g2d.setColor(shadowColor2);//new Color(0, 124, 36)
         g2d.fill(shadow);
         g2d.setTransform(reset);
 
